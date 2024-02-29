70361b39accd91a5ae910cc1004aa1f9810bd418
Atul Krishnan <atul.krishnan07@gmail.com>
2024-02-29 10:19:15 -0500

    Created a voice to text using google speech recongition to listen and save it to a file, open ai whisper to transcribe it, then delete the file

---
+++ VTT.py
@@ -0,0 +1,47 @@
+#! python3.7
+
+import argparse
+import os
+import numpy as np
+import speech_recognition as sr
+from openai import OpenAI
+
+import sensitiveData
+import warnings
+
+API_KEY = sensitiveData.apiKey
+client = OpenAI(api_key=API_KEY)
+
+def speech_to_text():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Say something...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source, timeout=4)
+
+    with open("audio_file.wav", "wb") as file:
+        file.write(audio.get_wav_data())
+
+    text = None
+    while text == None:
+        try:
+            print("Transcribing...")
+            audio_file = open("audio_file.wav", "rb")
+            transcription = client.audio.transcriptions.create(
+              model="whisper-1",
+              file=audio_file
+            )
+            text=transcription.text
+            print("You said:", text)
+            return text
+        except sr.UnknownValueError:
+            print("Could not understand audio. Try Again")
+        except sr.RequestError as e:
+            print(f"Error connecting to Google API: {e}")
+            break
+
+
+
+if __name__ == "__main__":
+    speech_to_text()
